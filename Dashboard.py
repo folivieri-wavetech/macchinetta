73c9dadb5c0030092fb272a6d3406a7277a4a6b3
marco.fiordok@gmail.com <marco.fiordok@gmail.com>
2026-08-26 03:25:38 +0200

    Rimosse bandiere dalla tabella Portafoglio IG

--- Dashboard.py
+++ Dashboard.py
@@ -100,24 +100,9 @@ def formatta_ultimo_evento_sintesi(msg, dati=None, nome=None):
     return msg
 
 def formatta_mercato_con_bandiere(nome):
-    flags = {
-        "AUD": "au",
-        "CAD": "ca",
-        "CHF": "ch",
-        "EUR": "eu",
-        "GBP": "gb",
-        "JPY": "jp",
-        "NZD": "nz",
-        "USD": "us"
-    }
-    
     if len(nome) == 7 and nome[3] == '/':
-        c1, c2 = nome[:3], nome[4:]
-        if c1 in flags and c2 in flags:
-            nome_clean = nome.replace("/", "")
-            img1 = f"<img src='https://flagcdn.com/w80/{flags[c1]}.png' width='54' style='border-radius:3px; box-shadow: 0 0 4px rgba(0,0,0,0.5); margin-bottom: 4px;'>"
-            img2 = f"<img src='https://flagcdn.com/w80/{flags[c2]}.png' width='54' style='border-radius:3px; box-shadow: 0 0 4px rgba(0,0,0,0.5); margin-top: 4px;'>"
-            return f"<div style='display: flex; flex-direction: column; align-items: center; line-height: 1.1; margin-left: 10px;'>{img1}<u style='color: #FFD700; font-size: 1.15em; font-weight: bold;'>{nome_clean}</u>{img2}</div>"
+        nome_clean = nome.replace("/", "")
+        return f"<div style='display: flex; flex-direction: column; align-items: center; line-height: 1.1; margin-left: 10px;'><u style='color: #FFD700; font-size: 1.15em; font-weight: bold;'>{nome_clean}</u></div>"
     
     return f"<u style='color: #FFD700; margin-left: 10px; font-weight: bold;'>{nome}</u>"
 
